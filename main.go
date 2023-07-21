8e0bd59c3421afde44b73b0036e44847f665a750
Achhaya Pathak <achhayapathak11@gmail.com>
2023-07-21 12:37:18 +0530

    Added Parent Node and Mapped Leaf Nodes

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+// 	Problem statement:
+
+// Implement a data structure for Merkle tree in Go where each leaf node represents a string value.
+// 1. Implement a method that takes an array of strings as input and returns an instance of the trie (to get Merkle root of the resulting trie)
+// 2. Implement a method to return a Merkle proof data structure that proves the existence of a string within the trie (can store references of leaf nodes in the Merkle trie data structure for the same)
+// 3. Use the generated proof to verify that the value actually exists in the said Merkle trie
+
 package main
 
 import (
@@ -10,22 +17,28 @@ type MerkleNode struct {
 	Hashvalue  []byte
 	LeftChild  *MerkleNode
 	RightChild *MerkleNode
-	// Parent *MerkleNode
+	Parent     *MerkleNode
 }
 
-func ConstructTree(values []string) *MerkleNode {
+func ConstructTree(values []string, LeafMap map[string]MerkleNode) *MerkleNode {
 	hashValues := []MerkleNode{}
+
 	for _, val := range values {
-		// hash values
+
 		sum := sha256.Sum256([]byte(val))
-		node := MerkleNode{sum[:], nil, nil}
+		node := MerkleNode{sum[:], nil, nil, nil}
+
 		hashValues = append(hashValues, node)
+
+		LeafMap[val] = node
 	}
-	
+
 	for len(hashValues) > 1 {
 		tempHashes := []MerkleNode{}
+
 		for i := 0; i < len(hashValues); i += 2 {
 			tempArray := []byte{}
+
 			tempArray = append(tempArray[:], hashValues[i].Hashvalue...)
 			if i+1 == len(hashValues) {
 				tempArray = append(tempArray[:], hashValues[i].Hashvalue...)
@@ -35,11 +48,16 @@ func ConstructTree(values []string) *MerkleNode {
 
 			sum := sha256.Sum256([]byte(tempArray))
 			var node MerkleNode
+
 			if i+1 == len(hashValues) {
-				node = MerkleNode{sum[:], &hashValues[i], &hashValues[i]}
+				node = MerkleNode{sum[:], &hashValues[i], &hashValues[i], nil}
+				hashValues[i].Parent = &node
 			} else {
-				node = MerkleNode{sum[:], &hashValues[i], &hashValues[i+1]}
+				node = MerkleNode{sum[:], &hashValues[i], &hashValues[i+1], nil}
+				hashValues[i].Parent = &node
+				hashValues[i+1].Parent = &node
 			}
+
 			tempHashes = append(tempHashes, node)
 		}
 
@@ -51,17 +69,11 @@ func ConstructTree(values []string) *MerkleNode {
 }
 
 func main() {
-
-	// 	Problem statement:
-
-	// Implement a data structure for Merkle tree in Go where each leaf node represents a string value.
-	// 1. Implement a method that takes an array of strings as input and returns an instance of the trie (to get Merkle root of the resulting trie)
-	// 2. Implement a method to return a Merkle proof data structure that proves the existence of a string within the trie (can store references of leaf nodes in the Merkle trie data structure for the same)
-	// 3. Use the generated proof to verify that the value actually exists in the said Merkle trie
+	LeafMap := make(map[string]MerkleNode)
 
 	Input := []string{"alice", "bob", "charlie", "david", "erin", "fiona", "george", "hannah"}
 
-	result := ConstructTree(Input)
+	result := ConstructTree(Input, LeafMap)
 
 	str := hex.EncodeToString(result.Hashvalue)
 
